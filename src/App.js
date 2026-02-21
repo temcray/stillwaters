b088a1c12635c749adf00b49b0a5870d1d9c9056
Tatiana Mccray <ejamison06@gmail.com>
2026-02-21 17:06:24 -0600

    Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Services from "./pages/Services";
-import Contact from "./pages/Contact";
+import React, { useState } from "react";
+import ResidentCard from "./components/ResidentCard";
+
 
 function App() {
+  const [residents, setResidents] = useState([
+    { id: 1, nmae: "john doe", age: 30, status: "Checked In" },
+    { id: 2, name: "jane smith", age: 25, status: "Checked Out"},
+    { id: 3, name: "bob johnson", age: 40, status: "Checked In" },
+  ]);
+
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+    <div style={{ padding: "20px" }}>
+      <h1>Still Waters Shelter Residents</h1>
+      {residents.map((resident) => 
+      (
+        <ResidentCard 
+        key={resident.id} 
+        name={resident.name} 
+        age={resident.age} 
+        status={resident.status} 
+        />
+      ))}
+
+      <button onClick={() =>
+      setResidents([
+        ...residents,
+        { id: residents.length + 1, 
+          name: "new resident", 
+          age: 35, status: "Checked In" },
+      ])
+
+      }>
+        Add Resident
+
+      </button>
+
+      <button onClick={() =>
+      setResidents([])}>
+        Clear Residents
+        </button>
+    </div>
   );
 }
 
 export default App;
-
 
